99e14a6357002df4b77a7dd314086b8c57f7ea71
Satoru Ohzawa <satoru@somba112013.local>
2016-09-22 23:40:55 +0900

    first commit

---
+++ build/webpack.dev.conf.js
@@ -0,0 +1,45 @@
+var config = require('../config')
+var webpack = require('webpack')
+var merge = require('webpack-merge')
+var utils = require('./utils')
+var baseWebpackConfig = require('./webpack.base.conf')
+var ExtractTextPlugin = require('extract-text-webpack-plugin')
+var HtmlWebpackPlugin = require('html-webpack-plugin')
+
+// add hot-reload related code to entry chunks
+Object.keys(baseWebpackConfig.entry).forEach(function (name) {
+  baseWebpackConfig.entry[name] = ['./build/dev-client'].concat(baseWebpackConfig.entry[name])
+})
+
+module.exports = merge(baseWebpackConfig, {
+  module: {
+    loaders: utils.styleLoaders({
+      sourceMap: config.dev.cssSourceMap,
+      extract: false,
+    })
+  },
+  vue: {
+    loaders: utils.cssLoaders({
+      sourceMap: config.dev.cssSourceMap,
+      extract: false,
+    })
+  },
+  // eval-source-map is faster for development
+  devtool: '#source-map',
+  plugins: [
+    new webpack.DefinePlugin({
+      'process.env': config.dev.env
+    }),
+    // https://github.com/glenjamin/webpack-hot-middleware#installation--usage
+    new webpack.optimize.OccurenceOrderPlugin(),
+    // new ExtractTextPlugin(utils.assetsPath('[name].css')),
+    new webpack.HotModuleReplacementPlugin(),
+    new webpack.NoErrorsPlugin(),
+    // https://github.com/ampedandwired/html-webpack-plugin
+    new HtmlWebpackPlugin({
+      filename: 'index.html',
+      template: 'public/index.template.html',
+      inject: true
+    })
+  ]
+})
